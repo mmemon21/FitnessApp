c7faf262ea244a223ad02d407d14ec8cdea060db
mmemon21 <mmemon@csumb.edu>
2020-12-05 12:55:20 -0800

    Some tests added fitness.java

--- app/src/androidTest/java/com/example/fitnessproject3fall/fitnesstests.java
+++ app/src/androidTest/java/com/example/fitnessproject3fall/fitnesstests.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import android.content.Context;
 
+import com.example.fitnessproject3fall.model.Coach;
 import com.example.fitnessproject3fall.model.FitnessDAO;
 import com.example.fitnessproject3fall.model.FitnessDB;
 import com.example.fitnessproject3fall.model.User;
@@ -31,7 +32,6 @@ public class fitnesstests {
     Context context = ApplicationProvider.getApplicationContext();
     private FitnessDB db;
     private FitnessDAO fitnessDAO = FitnessDB.getFitnessDB(context).dao();
-    User new_user = new User(3, "first_name", "last_name", 20, "", "", "user_name", "pass_word", 0);
 
     @Before
     public void createDatabase(){
@@ -45,8 +45,28 @@ public class fitnesstests {
     }
 
     @Test
-    public void addUserToDatabase() throws Exception {
+    public void addUserToDatabaseTest() throws Exception {
+        User new_user = new User(500, "testuser", "last_name", 20, "", "", "user_name", "pass_word", 100);
         fitnessDAO.addUser(new_user);
-        assertEquals(new_user.getFirst_name(), "first_name");
+        assertEquals(new_user.getFirst_name(), "testuser");
+        assertEquals(new_user.getAge(), 20);
+        assertEquals(new_user.getBio(), "");
+       // new_user = new User(300, "testuser", "last_name", 20, "", "", "user_name", "pass_word", 100);
+        fitnessDAO.deleteUser(new_user);
+    }
+
+    @Test
+    public void addCoachToDatabaseTest() throws Exception{
+        Coach new_coach = new Coach(6000, "Glenn", "Bruns", 40, "", "", "glenn22", "password", 2000);
+        fitnessDAO.addCoach(new_coach);
+        assertEquals(new_coach.getFirst_name(), "Glenn");
+        assertEquals(new_coach.getAge(), 40);
+        assertEquals(new_coach.getBio(), "");
+        fitnessDAO.deleteCoach(new_coach);
+    }
+
+    @Test //tbd
+    public void testMessages() throws Exception{
+
     }
 }
--- app/src/main/java/com/example/fitnessproject3fall/model/FitnessDAO.java
+++ app/src/main/java/com/example/fitnessproject3fall/model/FitnessDAO.java
@@ -3,6 +3,7 @@ package com.example.fitnessproject3fall.model;
 import java.util.List;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
@@ -83,4 +84,8 @@ public interface FitnessDAO {
 
     @Update
     void updateUser(User user);
+    @Delete
+    void deleteUser(User user);
+    @Delete
+    void deleteCoach(Coach coach);
 }
